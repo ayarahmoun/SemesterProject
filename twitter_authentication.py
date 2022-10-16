544db537ca75b7bcec829ede4658488153a86335
abdeslamG <abdeslam.guessous@epfl.ch>
2022-10-16 23:03:47 +0200

    Scraping Twitter Ok
    Le scrapping commence à fonctionner correctement. On arrive à prdouire un Df

---
+++ twitter_authentication.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+bearer_token='AAAAAAAAAAAAAAAAAAAAAHH9iAEAAAAAkMyMGd3LrN6sWA00oOgRc767oz8%3Dq1kBjW6Rq4Ksy6klsYkycODzv1g2sGdk01d2h2JtcXkk8LdNtn'
+#bearer_token="AAAAAAAAAAAAAAAAAAAAAKcJhgEAAAAA4BYVTjJb5%2BLWdaWaX1iCbpn%2Fzwo%3DBAq2AUbmTaArlP8GlSe0IEI8zCaWrYLHXrvP3CxM0XNn7j3Yhz"
+
+# In[ ]:
+
+
+
+
